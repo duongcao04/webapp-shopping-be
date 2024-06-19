2ef0e5925ec435bed8cf29b19df427fbb9e437ae
Cao Hai Duong <46746869+haiduongg@users.noreply.github.com>
2024-06-19 18:12:23 +0700

    ..

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,22 +1,28 @@
-const createError = require("http-errors");
+const createError = require('http-errors');
 
 const User = require('../Models/user.model');
 
 const { userValidate } = require('../helpers/validation');
-const { signAccessToken, signRefreshToken, verifyRefreshToken } = require('../helpers/jwt_services');
+const {
+    signAccessToken,
+    signRefreshToken,
+    verifyRefreshToken,
+} = require('../helpers/jwt_services');
 
 const userController = {
     //REGISTER USER
     registerUser: async (req, res, next) => {
         try {
-            const { error } = userValidate(req.body)
+            const { error } = userValidate(req.body);
             if (error) {
-                throw createError(error.details[0].message)
+                throw createError(error.details[0].message);
             }
 
-            const isExist = await User.findOne({ email: req.body.email })
+            const isExist = await User.findOne({ email: req.body.email });
             if (isExist) {
-                throw createError.Conflict(`${req.body.email} is ready been registered`)
+                throw createError.Conflict(
+                    `${req.body.email} is ready been registered`
+                );
             } else {
                 // Create new user
                 const newUser = new User({
@@ -30,26 +36,26 @@ const userController = {
                 //Save user to database
                 const user = await newUser.save();
 
-                res.status(200).json({ status: "isOkay", elements: user });
+                res.status(200).json({ status: 'isOkay', elements: user });
             }
         } catch (error) {
-            next(error)
+            next(error);
         }
     },
 
     //LOGIN
     loginUser: async (req, res, next) => {
         try {
-            const { error } = userValidate(req.body)
+            const { error } = userValidate(req.body);
             if (error) {
-                throw createError(error.details[0].message)
+                throw createError(error.details[0].message);
             }
 
             const user = await User.findOne({ email: req.body.email });
             if (!user) {
-                throw createError.NotFound(`User not registered`)
+                throw createError.NotFound(`User not registered`);
             }
-            const isValid = await user.isCheckedPasswrod(req.body.password)
+            const isValid = await user.isCheckedPasswrod(req.body.password);
             if (!isValid) {
                 throw createError.Unauthorized();
             }
@@ -58,12 +64,13 @@ const userController = {
             //Don't res password in object user
             const { password, ...others } = user._doc;
 
-            res.status(200).json({ status: "isOkay", elements: others, accessToken })
+            res
+                .status(200)
+                .json({ status: 'isOkay', elements: others, token: accessToken });
         } catch (error) {
-            next(error)
+            next(error);
         }
     },
-
 
     //REFRESH TOKEN
     refreshToken: async (req, res, next) => {
@@ -72,11 +79,17 @@ const userController = {
             if (!refreshToken) throw createError.BadRequest();
 
             const { userId, userRole } = await verifyRefreshToken(refreshToken);
-            const newAccessToken = await signAccessToken({ id: userId, role: userRole })
-            const newRefreshToken = await signRefreshToken({ id: userId, role: userRole })
-            res.json({newAccessToken,newRefreshToken})
+            const newAccessToken = await signAccessToken({
+                id: userId,
+                role: userRole,
+            });
+            const newRefreshToken = await signRefreshToken({
+                id: userId,
+                role: userRole,
+            });
+            res.json({ newAccessToken, newRefreshToken });
         } catch (error) {
-            next(error)
+            next(error);
         }
     },
 
@@ -84,10 +97,9 @@ const userController = {
     getAllUser: async (req, res, next) => {
         try {
             const user = await User.find();
-            res.status(200).json({ status: "isOkay", elements: user });
-        }
-        catch (error) {
-            next(error)
+            res.status(200).json({ status: 'isOkay', elements: user });
+        } catch (error) {
+            next(error);
         }
     },
 
@@ -95,23 +107,25 @@ const userController = {
     getOneUser: async (req, res, next) => {
         try {
             const user = await User.findById(req.params.id);
-            res.status(200).json({ status: "isOkay", elements: user });
-        }
-        catch (error) {
-            next(error)
+            res.status(200).json({ status: 'isOkay', elements: user });
+        } catch (error) {
+            next(error);
         }
     },
 
     //DELETE USER
     deleteUser: async (req, res, next) => {
         try {
-            const user = await User.findById(req.params.id)
-            res.status(200).json({ status: "isOkay", message: "Delete successfully", elements: user });
+            const user = await User.findById(req.params.id);
+            res.status(200).json({
+                status: 'isOkay',
+                message: 'Delete successfully',
+                elements: user,
+            });
+        } catch (error) {
+            next(error);
         }
-        catch (error) {
-            next(error)
-        }
-    }
+    },
 };
 
 module.exports = userController;
